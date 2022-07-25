d4a0ce50312a938fe362c061fe335579fc171eda
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-07-25 21:41:35 +0100

    Added Sort function

--- script.js
+++ script.js
@@ -61,11 +61,13 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-const displayMovements = function(movements) {
+const displayMovements = function(movements, sort = false) {
 
     containerMovements.innerHTML = ``;
 
-    movements.forEach(function(mov, i) {
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+    movs.forEach(function(mov, i) {
         const type = mov > 0 ? "deposit" : "withdrawal"; 
         const html = `
         <div class = "movements__row">
@@ -87,7 +89,7 @@ const displayMovements = function(movements) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, cur) => acc + cur, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${acc.balance} EUR`;
 };
 
 const calcDisplaySummary = function (acc) {
@@ -190,6 +192,16 @@ btnTransfer.addEventListener('click', function(event) {
 
     inputTransferAmount.value = inputTransferTo.value = ``;
 })
+
+//SORT EVENT HANDLER
+let sorted = false;
+
+btnSort.addEventListener('click', function(event) {
+    event.preventDefault();
+    displayMovements(currentAccount.movements, !sorted);
+    sorted = !sorted;
+    }
+);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
