a081b889699873b196fcec66fb4ea0e680227331
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-07-27 21:57:53 +0100

    Added starter files for linked webpage

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
-// BANKIST APP
+// BANKIST APP - ACCOUNT PAGE
 
 // Data
 const account1 = {
@@ -351,3 +351,35 @@ const totalDepositsUsd = movements
   .reduce((acc, mov) => acc + mov, 0);
 
 const firstwithdrawal = movements.find((mov) => mov < 0);
+
+///////////////////////////////////////
+//BANKIST APP - WEBPAGE FUNCTIONALITY
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
