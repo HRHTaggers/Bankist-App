e3478aaff1fbe639667e069d5a3dd25ed90f968f
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-07-30 09:27:14 +0100

    Added navigation scrolling functionality

--- script2.js
+++ script2.js
@@ -7,6 +7,8 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const header = document.querySelector(`.header`);
+const message = document.createElement(`div`);
 
 const openModal = function (event) {
   event.preventDefault();
@@ -30,26 +32,6 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
-//COOKIE MESSAGE
-const header = document.querySelector(`.header`);
-const message = document.createElement(`div`);
-
-message.classList.add(`cookie-message`);
-message.innerHTML = `We use cookies for improved functionality & analytics. <button class="btn btn--close-cookie">Got it!</button>`;
-header.prepend(message);
-document
-  .querySelector(`.btn--close-cookie`)
-  .addEventListener("click", function () {
-    message.remove();
-  });
-
-message.style.backgroundColor = `#5ec576`;
-message.style.color = `white`;
-message.style.width = `120%`;
-
-const btnScrollto = document.querySelector(`.btn--scroll-to`);
-const section1 = document.querySelector(`#section--1`);
-
 //SCROLL BEHAVIOUR
 btnScrollto.addEventListener(`click`, function() {
 
@@ -65,3 +47,29 @@ btnScrollto.addEventListener(`click`, function() {
     section1.scrollIntoView( {behavior: `smooth`} );
 });
 
+//PAGE NAVIGATION
+document.querySelector(`.nav__links`).addEventListener(`click`, function(event) {
+    event.preventDefault();
+
+    if(event.target.classList.contains(`nav__link`)) {
+        const id = event.target.getAttribute(`href`);
+        document.querySelector(id).scrollIntoView({ behavior: `smooth` });
+    }
+});
+
+//COOKIE MESSAGE
+message.classList.add(`cookie-message`);
+message.innerHTML = `We use cookies for improved functionality & analytics. <button class="btn btn--close-cookie">Got it!</button>`;
+header.prepend(message);
+document
+  .querySelector(`.btn--close-cookie`)
+  .addEventListener("click", function () {
+    message.remove();
+  });
+
+message.style.backgroundColor = `#5ec576`;
+message.style.color = `white`;
+message.style.width = `120%`;
+
+const btnScrollto = document.querySelector(`.btn--scroll-to`);
+const section1 = document.querySelector(`#section--1`);
